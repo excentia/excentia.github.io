bd948ca6351d22468ca15e2f336fc969332317aa
acalero <acalero@excentia.es>
2021-01-02 13:27:14 +0100

    Home page redesigned. Links are not included yet

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
 
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
 
 	$("#testimonial-slider").owlCarousel({
 	    paginationSpeed : 800,      
 	    singleItem:true,
 	    autoPlay: 8000,
 	});
+
+	$("#customer-logos").owlCarousel({
+  		autoPlay: 3000,
+  		items : 7,
+  		itemsDesktop : [1199,5],
+  		itemsDesktopSmall : [979,5],
+  	});
     
     $("#product-features-slider").owlCarousel({
 	    paginationSpeed : 800,      
